0704ac7427ffed024c33fe3ab12bff097e4849ba
Mac <mohammed.ali.chherawalla@gmail.com>
2020-05-20 14:10:59 +0530

    transpile to es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,60 @@
-const path = require("path");
-const webpack = require("webpack");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "react-screentype-hook.js",
-    library: "react-screentype-hook",
-    libraryTarget: "umd"
-  },
-  externals: {
-    lodash: {
-      commonjs: "lodash",
-      commonjs2: "lodash",
-      amd: "lodash",
-      root: "_"
-    },
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
-      root: "react"
-    }
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-        terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-        }
-      })
-    ]
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: "development"
-    })
-  ],
-  resolve: {
-    modules: ["node_modules", "app"],
-    extensions: [".js", ".jsx", ".react.js"],
-    mainFields: ["browser", "jsnext:main", "main"]
-  }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'lib'),
+		filename: 'react-screentype-hook.js',
+		library: 'react-screentype-hook',
+		libraryTarget: 'umd'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/, // Transform all .js and .jsx files required somewhere with Babel
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	},
+	externals: {
+		lodash: {
+			commonjs: 'lodash',
+			commonjs2: 'lodash',
+			amd: 'lodash',
+			root: '_'
+		},
+		react: {
+			commonjs: 'react',
+			commonjs2: 'react',
+			amd: 'react',
+			root: 'react'
+		}
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true,
+				terserOptions: {
+					// https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+				}
+			})
+		]
+	},
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: 'development'
+		})
+	],
+	resolve: {
+		modules: ['node_modules', 'app'],
+		extensions: ['.js', '.jsx', '.react.js'],
+		mainFields: ['browser', 'jsnext:main', 'main']
+	}
 };
