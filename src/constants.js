c487ce1d8fbafbbab55010e9c41e088bf89db8a9
Chris <christin@wednesday.is>
2022-05-10 00:42:18 +0530

    type: Add type def for useScreenType refactor: Add prettier config
    Signed-off-by: Chris <christin@wednesday.is>

--- src/constants.js
+++ src/constants.js
@@ -2,22 +2,22 @@ export const DEFAULT_BREAKPOINTS = {
   largeDesktop: 1440,
   desktop: 992,
   tablet: 768,
-  mobile: 320
+  mobile: 320,
 };
 export const BREAKPOINT_TYPES = {
   mobile: "MOBILE",
   tablet: "TABLET",
   desktop: "DESKTOP",
-  largeDesktop: "LARGE_DESKTOP"
+  largeDesktop: "LARGE_DESKTOP",
 };
-export const getCurrentScreenType = currentScreenType => ({
+export const getCurrentScreenType = (currentScreenType) => ({
   isMobile: currentScreenType === BREAKPOINT_TYPES.mobile,
   isTablet: currentScreenType === BREAKPOINT_TYPES.tablet,
   isDesktop: currentScreenType === BREAKPOINT_TYPES.desktop,
-  isLargeDesktop: currentScreenType === BREAKPOINT_TYPES.largeDesktop
+  isLargeDesktop: currentScreenType === BREAKPOINT_TYPES.largeDesktop,
 });
 
-export const calculateCurrentScreenType = breakpoints => ({
+export const calculateCurrentScreenType = (breakpoints) => ({
   isMobile: window.innerWidth < breakpoints.tablet,
   isTablet:
     window.innerWidth >= breakpoints.tablet &&
@@ -25,5 +25,5 @@ export const calculateCurrentScreenType = breakpoints => ({
   isDesktop:
     window.innerWidth >= breakpoints.desktop &&
     window.innerWidth < breakpoints.largeDesktop,
-  isLargeDesktop: window.innerWidth >= breakpoints.largeDesktop
+  isLargeDesktop: window.innerWidth >= breakpoints.largeDesktop,
 });
